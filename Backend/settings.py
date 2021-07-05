656a0e0ca5a3acc0630581766c26e36cb69a81d2
Senpumaru <hakumen737@gmail.com>
2021-07-05 10:23:47 +0300

    0.1 Backend Release

--- Backend/settings.py
+++ Backend/settings.py
@@ -10,11 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import os
 from pathlib import Path
 from decouple import config
 from datetime import timedelta
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = "django-insecure-fs_u)o4+$aue)@yg%i$i2o57(zoqwv6c%y58-mh(g72(5pbf%e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["acs-test-app.herokuapp.com"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -54,12 +52,10 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    'corsheaders.middleware.CorsPostCsrfMiddleware',
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -91,29 +87,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "Backend.wsgi.application"
 
-CORS_ALLOWED_ORIGINS = [
-    "https://acs-project-8358d.firebaseapp.com",
-    "https://acs-project-8358d.web.app",
-]
+CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ALLOW_CREDENTIALS = True
-
-CORS_ORIGIN_WHITELIST = [
-    "https://acs-project-8358d.firebaseapp.com",
-    "https://acs-project-8358d.web.app",
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    "https://acs-project-8358d.firebaseapp.com",
-    "https://acs-project-8358d.web.app",
-]
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {}
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "ACS",
+        "USER": "zen",
+        "PASSWORD": "oo9Kj6jz*Cn3Dr",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    },
+}
 
 AUTH_USER_MODEL = "Account.ServiceUser"
 
@@ -179,9 +168,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
