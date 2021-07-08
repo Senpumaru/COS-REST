841cb369bbdbf3b2f7b8aa5419f80d3c33405055
Senpumaru <hakumen737@gmail.com>
2021-07-08 13:08:03 +0300

    0.2 - Decouple

--- Backend/settings.py
+++ Backend/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-fs_u)o4+$aue)@yg%i$i2o57(zoqwv6c%y58-mh(g72(5pbf%e"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -94,10 +94,10 @@ CORS_ORIGIN_ALLOW_ALL = True
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "ACS",
-        "USER": "zen",
-        "PASSWORD": "oo9Kj6jz*Cn3Dr",
-        "HOST": "127.0.0.1",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
         "PORT": "5432",
     },
 }
